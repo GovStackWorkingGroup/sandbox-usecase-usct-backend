b7a3b7ca93994294b0a61e71c5e1c75845415800
oleksiidn <oleksii.danyliuk@gofore.com>
2023-05-29 14:12:29 +0300

    Init Mock-SRIS implementation

---
+++ src/main/java/global/govstack/mocksris/MockSrisApplication.java
@@ -0,0 +1,13 @@
+package global.govstack.mocksris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockSrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockSrisApplication.class, args);
+	}
+
+}
--- src/test/java/global/govstack/mocksris/MockSrisApplicationTests.java
+++ src/test/java/global/govstack/mocksris/MockSrisApplicationTests.java
@@ -1,13 +1,13 @@
-package com.govstack.portal.backend;
+package global.govstack.mocksris;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class MockSrisApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
