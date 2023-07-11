3f00362d360901d5ebd2d1980f18a6ac1fcf2da8
oleksiidn <oleksii.danyliuk@gofore.com>
2023-07-11 15:28:50 +0300

    Added more candidates. SQL seeds.

--- src/main/resources/db/migration/V1_0__init_tables.sql
+++ src/main/resources/db/migration/V1_0__init_tables.sql
@@ -83,13 +83,37 @@ create table BENEFICIARY
 INSERT INTO PERSON(foundational_id, first_name, last_name, email, date_of_birth, bank_account_owner_name, financial_address, financial_modality, iban, bank_name)
 values ( '9b237f8a-4dc2-4438-af0d-5f01c469b302', 'John', 'Smith', 'john.smith@example.com','28-04-1977',  'John Smith', 'Wilhelmstraße 12', 'Bank Account',
          'DE12 3456 7812 3456 7890', 'Golden Bank' ),
-       ('9b237f8a-4dc2-4438-af0d-5f01c469b302', 'Emma', 'Smith', 'emma.smith@example.com','28-04-1977',  'Emma Smith', 'Wilhelmstraße 12', 'Bank Account',
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b303', 'Liam', 'Anderson', 'liam.anderson@example.com','28-04-1977',  'Liam Anderson', 'Wilhelmstraße 12', 'Bank Account',
         'DE12 3456 7812 3456 7890', 'Golden Bank'),
-       ('9b237f8a-4dc2-4438-af0d-5f01c469b302', 'Bob', 'Smith', 'bob.smith@example.com','28-04-1977',  'Bob Smith', 'Wilhelmstraße 12', 'Bank Account',
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b304', 'Olivia', 'Ramirez', 'olivia.ramirez@example.com','28-04-1977',  'Olivia Ramirez', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b305', 'Ava', 'Patel', 'ava.patel@example.com','28-04-1977',  'Ava Patel', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b306', 'William', 'Roberts', 'william.roberts@example.com','28-04-1977',  'William Roberts', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b307', 'Sophia', 'Campbell', 'sophia.campbell@example.com','28-04-1977',  'Sophia Campbell', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b308', 'Benjamin', 'Mitchell', 'benjamin.Mitchell@example.com','28-04-1977',  'Benjamin Mitchell', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b309', 'Isabella', 'Turner', 'isabella.turner@example.com','28-04-1977',  'Isabella Turner', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b310', 'Emma', 'Smith', 'emma.smith@example.com','28-04-1977',  'Emma Smith', 'Wilhelmstraße 12', 'Bank Account',
+        'DE12 3456 7812 3456 7890', 'Golden Bank'),
+       ('9b237f8a-4dc2-4438-af0d-5f01c469b311', 'Bob', 'Smith', 'bob.smith@example.com','28-04-1977',  'Bob Smith', 'Wilhelmstraße 12', 'Bank Account',
         'DE12 3456 7812 3456 7890', 'Golden Bank');
 
 
-INSERT INTO CANDIDATE(PERSON_ID) values ( 1 );
+INSERT INTO CANDIDATE(PERSON_ID) values
+                                     ( 1 ),
+                                     ( 2 ),
+                                     ( 3 ),
+                                     ( 4 ),
+                                     ( 5 ),
+                                     ( 6 ),
+                                     ( 7 ),
+                                     ( 8 ),
+                                     ( 9 ),
+                                     ( 10 );
 
 INSERT INTO PACKAGE (NAME, DESCRIPTION, AMOUNT, CURRENCY)
 VALUES ('UBI', 'Universal basic income', 1234.5, 'EURO'),
@@ -100,7 +124,30 @@ VALUES ('UBI', 'Universal basic income', 1234.5, 'EURO'),
 INSERT INTO CANDIDATE_PACKAGE (candidate_id, package_id)
 VALUES (1, 1),
        (1, 2),
-       (1, 3);
+       (1, 3),
+       (2, 1),
+       (2, 3),
+       (3, 1),
+       (3, 2),
+       (3, 3),
+       (3, 4),
+       (4, 1),
+       (5, 1),
+       (5, 2),
+       (5, 3),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (6, 4),
+       (7, 1),
+       (7, 2),
+       (7, 3),
+       (8, 1),
+       (8, 2),
+       (8, 3),
+       (9, 1),
+       (9, 2),
+       (9, 3);
 
 INSERT INTO HOUSEHOLD_INFORMATION (candidate_id, relative_id, relationship_type)
 VALUES (1, 2, 'WIFE'),
