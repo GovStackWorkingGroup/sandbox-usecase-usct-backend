5f4a62afbd9f87c321c9874aba06f924c1f49323
Valentin Filyov <valentin.filyov@athlonsofia.com>
2023-08-18 16:00:20 +0300

    SND-510: Connection of Digital registries BB emulator to mock-sris

---
+++ src/main/java/global/govstack/mocksris/configuration/DigitalRegistriesBBInformationMediatorProperties.java
@@ -0,0 +1,7 @@
+package global.govstack.mocksris.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "digitalregistries.bb.im")
+public record DigitalRegistriesBBInformationMediatorProperties(String baseUrl, String header) {}
