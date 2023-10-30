aa6a1ec92dce77d367443f24ba64af099924fc29
Oleksii <oleksiidn@protonmail.com>
2023-10-30 20:57:47 +0200

    Add logs

--- src/main/java/global/govstack/mocksris/service/OpenImisService.java
+++ src/main/java/global/govstack/mocksris/service/OpenImisService.java
@@ -3,17 +3,21 @@ package global.govstack.mocksris.service;
 import global.govstack.mocksris.configuration.OpenImisProperties;
 import global.govstack.mocksris.controller.dto.OpenImisPackageSet;
 import global.govstack.mocksris.controller.dto.PackageDto;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.utils.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class OpenImisService {
 
@@ -26,28 +30,36 @@ public class OpenImisService {
     }
 
     public List<PackageDto> getAll() {
-        OpenImisPackageSet packagesSet =
-                restTemplate
-                        .exchange(openImisProperties.url(),
-                                HttpMethod.GET,
-                                new HttpEntity<>(createHeaders("admin", "govstack")),
-                                OpenImisPackageSet.class)
-                        .getBody();
-
-        return packagesSet.results().stream().map(PackageDto::new).toList();
+        log.info("Get all packages from OpenIMIS");
+        try {
+            OpenImisPackageSet packagesSet =
+                    restTemplate
+                            .exchange(openImisProperties.url(),
+                                    HttpMethod.GET,
+                                    new HttpEntity<>(createHeaders("admin", "govstack")),
+                                    OpenImisPackageSet.class)
+                            .getBody();
+            return packagesSet.results().stream().map(PackageDto::new).toList();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
+        }
     }
 
     public Optional<PackageDto> getById(int id) {
+        log.info("Get package from OpenIMIS");
+        try {
+            OpenImisPackageSet packagesSet =
+                    restTemplate
+                            .exchange(openImisProperties.url(),
+                                    HttpMethod.GET,
+                                    new HttpEntity<>(createHeaders("admin", "govstack")),
+                                    OpenImisPackageSet.class)
+                            .getBody();
 
-        OpenImisPackageSet packagesSet =
-                restTemplate
-                        .exchange(openImisProperties.url(),
-                                HttpMethod.GET,
-                                new HttpEntity<>(createHeaders("admin", "govstack")),
-                                OpenImisPackageSet.class)
-                        .getBody();
-
-        return packagesSet.results().stream().map(PackageDto::new).findFirst();
+            return packagesSet.results().stream().map(PackageDto::new).findFirst();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage());
+        }
     }
 
     HttpHeaders createHeaders(String username, String password) {
